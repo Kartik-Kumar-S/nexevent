4205ed7b1f9adfd9cdbf95cc4294401469492c4a
Astha Batra <asthabatra018@gmail.com>
2026-03-20 21:33:56 +0530

    Added event_app and updated settings

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'event_app',
 
     # ---- Third-party ----
     'rest_framework',
